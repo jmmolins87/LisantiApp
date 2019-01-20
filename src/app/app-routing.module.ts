3bf3f5c15639e9dd836e29f1c4ab975cc9ed10c5
jmmcDesign <hola@jmmcdesign.com>
2019-01-20 22:16:41 +0100

    add router links on new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { DesayunoComponent } from './components/desayuno/desayuno.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { VinoComponent } from './components/vino/vino.component';
+import { CopasComponent } from './components/copas/copas.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'desayuno', component: DesayunoComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'vino', component: VinoComponent },
+  { path: 'copas', component: CopasComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
