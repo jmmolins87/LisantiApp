b91fa31925305f8c7b8780d854b1bf36c49f5ef7
jmmcDesign <hola@jmmcdesign.com>
2019-01-21 21:01:43 +0100

    add effects on header

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -3,14 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  host: {
+    '(window:scroll)': 'onScroll($event)'
+  }
 })
 export class NavbarComponent implements OnInit {
 
   abrirMenu: boolean  = false;
   abrirSubmenu: boolean = false;
 
+
+  // =========================
+  isScrolled = false;
+  currPos: Number = 0;
+  startPos: Number = 0;
+  changePos: Number = 0;
+
   constructor() { }
+
+  onScroll(evt) {
+    //window object can be wrapper in a service but for now we directly use it
+    this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
+    if(this.currPos >= this.changePos ) {
+        this.isScrolled = true;
+    } else {
+        this.isScrolled = false;
+    }
 
   ngOnInit() {
   }
