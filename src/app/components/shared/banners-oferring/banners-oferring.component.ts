c4bc291d2399ceb3404554d4be8c7bd7b081bd4b
jmmcDesign <hola@jmmcdesign.com>
2019-02-11 20:02:20 +0100

    create banner for ofering components

---
+++ src/app/components/shared/banners-oferring/banners-oferring.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banners-oferring',
+  templateUrl: './banners-oferring.component.html',
+  styleUrls: ['./banners-oferring.component.scss']
+})
+export class BannersOferringComponent implements OnInit {
+
+  images: any[];
+
+  constructor() { 
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
