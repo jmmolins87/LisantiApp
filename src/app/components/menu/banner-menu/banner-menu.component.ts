df15e0ec6efc81f8d61036920815893936639c14
jmmcDesign <hola@jmmcdesign.com>
2019-02-17 18:03:42 +0100

    create banner components for ofering components

---
+++ src/app/components/menu/banner-menu/banner-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-menu',
+  templateUrl: './banner-menu.component.html',
+  styles: []
+})
+export class BannerMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
