f2bbc206f22aac60f89b4d601fb025171fa43b2d
Juanma <jmmolins87@gmail.com>
2019-03-14 08:56:07 +0100

    repair navbar componenet

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -21,30 +21,7 @@ export class NavbarComponent implements OnInit {
   // changePos: Number = 0;
 
   constructor() { }
-
-<<<<<<< HEAD
   ngOnInit() {
   }
 
-  onScroll(evt) {
-    // window object can be wrapper in a service but for now we directly use it
-    this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
-    if (this.currPos >= this.changePos) {
-      this.isScrolled = true;
-    } else {
-      this.isScrolled = false;
-    }
-=======
-  // onScroll(evt) {
-  //   //window object can be wrapper in a service but for now we directly use it
-  //   this.currPos = (window.pageYOffset || evt.target.scrollTop) - (evt.target.clientTop || 0);
-  //   if(this.currPos >= this.changePos ) {
-  //       this.isScrolled = true;
-  //   } else {
-  //       this.isScrolled = false;
-  //   }
->>>>>>> master
-
-
-  }
 }
